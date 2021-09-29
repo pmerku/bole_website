4a3c49871da440f57fbd9d4d0aa0dc71326803de
pmerku <pmerku@gmail.com>
2021-09-29 18:16:02 +0200

    google maps integration

---
+++ bole-client/src/components/Map.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
+import './Map.css';
+
+const ContainerStyle = {
+  width: '800px',
+  height: '600px',
+};
+
+const options = {
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+
+export const Map = React.memo(function Map(props: {
+  location: any;
+  zoom: number;
+  isMarkerShown?: boolean;
+}) {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: window._env_.BOLE_GOOGLE_API_KEY,
+    libraries: ['places'],
+  });
+
+  function showMarker() {
+    // TODO get zoom of map and hide marker when zoom below 15
+  }
+
+  if (loadError) return <div>Error loading map</div>;
+  if (!isLoaded) return <div>Map is still loading...</div>;
+
+  return (
+    <div className="map-wrapper">
+      <h2 className="map-title">Come visit us at here</h2>
+      <div className="map">
+        <GoogleMap
+          mapContainerStyle={ContainerStyle}
+          center={props.location}
+          zoom={props.zoom}
+          options={options}
+          onZoomChanged={showMarker}
+        >
+          {props.isMarkerShown ? (
+            <Marker
+              position={{
+                lat: props.location?.lat,
+                lng: props.location?.lng,
+              }}
+            />
+          ) : null}
+        </GoogleMap>
+      </div>
+    </div>
+  );
+});
--- bole-client/src/views/ContactsView.tsx
+++ bole-client/src/views/ContactsView.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import './ContactsView.css';
+import { Map } from '../components/Map';
+
+const location = {
+  address: 'Via Sottomonte, 25, 34135 Trieste TS',
+  lat: 45.67296,
+  lng: 13.77905,
+};
 
 export function ContactsView() {
-  return <div></div>;
+  return (
+    <div className="contacts-wrapper">
+      <div className="map-container">
+        <Map location={location} zoom={15} isMarkerShown />
+      </div>
+    </div>
+  );
 }
